2922fd1df13d185f04e16429dfd5b930ab00315f
Nícolas Carvalho <nicolas.mnw@gmail.com>
2024-01-29 13:12:17 -0300

    Added redirectLoader

--- webpage/src/routes.tsx
+++ webpage/src/routes.tsx
@@ -1,5 +1,5 @@
 import './index.css'
-import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { Route, createBrowserRouter, createRoutesFromElements, redirect } from 'react-router-dom'
 import { RouterProvider } from 'react-router';
 import { Post } from './pages/post/post';
 
@@ -11,9 +11,13 @@ import { Login } from './pages/auth/login';
 import { Register } from './pages/auth/register';
 import { UserFeed } from './pages/user/userFeed';
 
+const redirectLoader = (path: string) => async () => redirect(path);
+
 const router = createBrowserRouter(
   createRoutesFromElements(<>
-    <Route path="/" element={<Root />}>
+    <Route path="/" loader={redirectLoader("/home")}></Route>
+    <Route element={<Root />}>
+      <Route path="feed" loader={redirectLoader("recent")}></Route>
       <Route path="feed" element={<Feed />}>
         <Route path="recent" element={<Recent />}></Route>
         <Route path="category" element={<Category />}></Route>
